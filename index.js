eb75ef68b7f90e228feb986497629d9afe798e28
0aoq <73413330+0aoq@users.noreply.github.com>
2022-02-09 14:55:37 -0500

    .

--- index.js
+++ index.js
@@ -203,7 +203,7 @@ setInterval(() => {
         window.localStorage.setItem("currentFormI", 0)
     }
 
-    if (window.localStorage.getItem("forms") !== "null") {
+    if (window.localStorage.getItem("forms") !== "null" && window.localStorage.getItem("forms") !== null) {
         if (JSON.parse((window.localStorage.getItem('forms'))).length >= 50) {
             alert("You have too many forms. They will be removed when you click OK.")
             window.localStorage.setItem("forms", JSON.stringify([]))
